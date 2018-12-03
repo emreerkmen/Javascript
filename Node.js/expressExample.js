90b8207a7ea90e257c2e46e0d3775b9cf8ed3bde
Emre Erkmen <emre.erkmen@hotmail.com>
2018-12-04 00:37:38 +0300

    module

--- Node.js/expressExample.js
+++ Node.js/expressExample.js
@@ -2,28 +2,33 @@ var fs = require('fs');
 var p = require('./exportsDemo.js');
 var express = require('express');
 var path=require('path');
+var ctrlElektronik = require('./elektronikController');
 
 var app = express();
 
 app.use('/public',express.static(path.join(__dirname, 'public')));
 
-app.get('/',function(req,res){
-    fs.readFile('home.html', function (err, data) {
-        res.write(data)
-        res.end('Sayfa Sonu');
-        p('Home Controller');
-    });
-}
+// var homeController = function(req,res){
+//     // fs.readFile('home.html', function (err, data) {
+//     //     res.write(data)
+//     //     res.end('Sayfa Sonu');
+//     //     p('Home Controller');
+//     // });
+//     res.sendFile(path.join(__dirname, 'home.html'));
+// };
 
-);
+// var adminController = function(req,res){
+//     // fs.readFile('admin.html',function(err,data){
+//     //     res.write(data);
+//     //     res.end('Sayfa Sonu');
+//     //     p('Admin Controller');
+//     // })
+//     res.sendFile(path.join(__dirname, 'admin.html'));
+// };
 
-app.get('/admin',function(req,res){
-    fs.readFile('admin.html',function(err,data){
-        res.write(data);
-        res.end('Sayfa Sonu');
-        p('Admin Controller');
-    })
-});
+app.get('/',ctrlElektronik.homeController);
+
+app.get('/admin',ctrlElektronik.adminController);
 
 
 
